759c3b6f262cd755f8883d24c1e96464a742d4b7
PacioTenPros <paciobumbum@gmail.com>
2022-10-18 22:51:05 +0200

    nav options

--- App.js
+++ App.js
@@ -2,11 +2,14 @@ import { StyleSheet } from 'react-native';
 import { Provider } from 'react-redux';
 import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <HomeScreen />
+      <SafeAreaProvider>
+        <HomeScreen />
+      </SafeAreaProvider>
     </Provider>
   );
 };
--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,11 +1,51 @@
-import { View, Text } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity, Image } from 'react-native';
 import React from 'react';
+import tw from 'twrnc';
+import { Icon } from 'react-native-elements';
+
+const DATA = [
+  {
+    id: '0',
+    title: 'Get a ride',
+    image: '3pn',
+    screen: 'MapScreen',
+  },
+  {
+    id: '1',
+    title: 'Order food',
+    image: '28w',
+    screen: 'EatsScreen', // in the future
+  },
+];
 
 const NavOptions = () => {
   return (
-    <View>
-      <Text>NavOptions</Text>
-    </View>
+    <FlatList
+      data={DATA}
+      horizontal
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => (
+        <TouchableOpacity
+          style={tw`pr-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40 rounded-xl`}
+        >
+          <View>
+            <Image
+              style={{ width: 120, height: 120, resizeMode: 'contain' }}
+              source={{
+                uri: `https://links.papareact.com/${item.image}`,
+              }}
+            />
+            <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+            <Icon
+              type='antdesign'
+              name='arrowright'
+              color='white'
+              style={tw`p-2 bg-gray-800 rounded-full w-10 mt-4`}
+            />
+          </View>
+        </TouchableOpacity>
+      )}
+    />
   );
 };
 
