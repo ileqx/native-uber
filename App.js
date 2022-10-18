a321f59ea1774d4d7e2b39d77b2ffcc840547ca8
PacioTenPros <paciobumbum@gmail.com>
2022-10-18 22:34:32 +0200

    tailwind + rn web

--- App.js
+++ App.js
@@ -1,15 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { Provider } from 'react-redux';
+import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Lets build uber!</Text>
-        <StatusBar style='auto' />
-      </View>
+      <HomeScreen />
     </Provider>
   );
 };
